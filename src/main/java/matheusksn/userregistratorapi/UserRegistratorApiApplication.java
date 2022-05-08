6888ab7e048512a68e8cab0aaefd16dc9475aa63
matheusksn <ksn.matheus@gmail.com>
2022-05-08 16:29:28 -0300

    first commit

---
+++ src/main/java/matheusksn/userregistratorapi/UserRegistratorApiApplication.java
@@ -0,0 +1,13 @@
+package matheusksn.userregistratorapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserRegistratorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserRegistratorApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/matheusksn/userregistratorapi/UserRegistratorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package matheusksn.userregistratorapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserRegistratorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
